83f9fcc2b34cc4f6b80d968c687b991175175d61
Kyzyl-ool Kezhik <kyzyloolk@mail.ru>
2020-07-25 12:55:26 +0300

    storybook configured for TS

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,15 @@
+const myConfig = require('../webpack.config.js');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async config => {
-    // do mutation to the config
-
-    return config;
-  },
+  return {
+    ...config,
+    module: {
+      rules: myConfig.module.rules
+    },
+    resolve: myConfig.resolve
+  };
+},
 };
